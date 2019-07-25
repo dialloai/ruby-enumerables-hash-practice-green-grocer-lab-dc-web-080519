59f6401c1cce73fe4b753312e23de633abed1746
Aicha D <aicha1diallo@gmail.com>
2019-07-25 21:16:22 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -5,7 +5,7 @@ def consolidate_cart(cart)
   m=x.keys[0]
   new_hash[m]= x[m]
   new_hash[m][:count]=1 
-  if new_hash
+  if 
   
   # if the current item already exists in new_hash      
   # increment the count by one  
