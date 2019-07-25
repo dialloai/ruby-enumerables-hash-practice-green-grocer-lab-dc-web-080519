0c0696b930d478a54ba96991bd91629513d99f15
Aicha D <aicha1diallo@gmail.com>
2019-07-25 21:10:25 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,6 +1,22 @@
+require "pry"
 def consolidate_cart(cart)
-  # code here
+  new_hash={}
+  cart.each do|x|
+  m=x.keys[0]
+  new_hash[m]= x[m]
+  new_hash[m][:count]=1 
+  
+  # if the current item already exists in new_hash      
+  # increment the count by one  
+  # if it doesnt already exist in the new_hash      
+  # create the item in the new_hash 
+ # create the :count key and set the value = 1 end 
+  
+# binding.pry
+ end
+ return new_hash
 end
+
 
 def apply_coupons(cart, coupons)
   # code here
